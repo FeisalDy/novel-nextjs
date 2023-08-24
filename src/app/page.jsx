7358a50610d837956ebaf94900bc392b1a227978
feisaldy <feisaldy26@gmail.com>
2023-08-24 22:36:26 +0700

    fix server side

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,23 @@
-'use client'
+// 'use client'
+// import React, { useEffect, useState } from 'react'
 import Button from '@/components/Button'
 import Label from '@/components/Label'
-import React, { useEffect } from 'react'
 import axios from '@/lib/axios'
 import Link from 'next/link'
-import { useState } from 'react'
 
-const Home = () => {
-    const [novel, setNovel] = useState([])
+const getNovels = async () => {
+    const novels = await fetch('http://novel-backend.test/api/novels?page=1')
 
-    useEffect(() => {
-        const getNovels = async () => {
-            try {
-                const novels = await axios.get('/api/novels')
-                const novelsData = await novels.data.data
+    const novelsData = await novels.json()
+    console.log(novelsData)
 
-                console.log(novelsData)
-                setNovel(novelsData)
-            } catch (error) {
-                console.error('Error fetching menu data:', error)
-            }
-        }
-        getNovels()
-    }, [])
+    return novelsData
+}
+
+const Home = async () => {
+    const novelData = await getNovels()
+    const novel = await novelData.data
+    console.log(novel)
 
     return (
         <div>
@@ -35,12 +30,33 @@ const Home = () => {
     )
 }
 
-export default Home
-
 // const Home = () => {
-//   return (
-//     <div>Home</div>
-//   )
+//     const [novel, setNovel] = useState([])
+
+//     useEffect(() => {
+//         const getNovels = async () => {
+//             try {
+//                 const novels = await axios.get('/api/novels?page=1')
+//                 const novelsData = await novels.data.data
+
+//                 console.log(novelsData)
+//                 setNovel(novelsData)
+//             } catch (error) {
+//                 console.error('Error fetching menu data:', error)
+//             }
+//         }
+//         getNovels()
+//     }, [])
+
+//     return (
+//         <div>
+//             {novel.map(data => (
+//                 <div key={data.id}>
+//                     <a href=''>{data.title}</a>
+//                 </div>
+//             ))}
+//         </div>
+//     )
 // }
 
-// export default Home
+export default Home
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -8,4 +8,20 @@ const axios = Axios.create({
     withCredentials: true
 })
 
+// // Function to fetch CSRF token
+// async function fetchCsrfToken () {
+//     // Make your API call to fetch the CSRF token from the server
+//     const response = await axios.get('/sanctum/csrf-cookie') // Adjust the endpoint according to your API
+
+//     // Assuming the CSRF token is returned in response.data.csrfToken
+//     return response.data.csrfToken
+// }
+
+// // Set the CSRF token in the Axios instance headers
+// axios.interceptors.request.use(async config => {
+//     const csrfToken = await fetchCsrfToken()
+//     config.headers['X-CSRF-TOKEN'] = csrfToken
+//     return config
+// })
+
 export default axios
